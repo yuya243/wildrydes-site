ffd5bd83c948845570a479c10fa93d99c0497e11
yuya243 <yuya-takahashi@telema.jp>
2025-07-28 15:43:00 +0900

    change_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_RXrI2w5qJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6qkqp4e8blbk6c20v8tkt115n7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-1_WCgMPZKCH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '484jnvhp6kujs4hk9cvd1p5ql7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
